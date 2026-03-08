a2b3d5f64ca68351ea9b4e36ba95e8da797b2490
Liberacel <alexbussiness2019@gmail.com>
2026-03-08 00:10:09 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert (Bienvenido a mi pagina web);
+alert (Bienvenido, a, mi, pagina, web);
+function cambiarTexto(){
+document.getElementById("mensaje").innerHTML = "Gracias por visitar mi página";
+}
+function modoOscuro(){
+document.body.style.background = "black";
+document.body.style.color = "white";
+}
